242c80dd9bb296ad5a2196719960cf97346c0001
GRETA <mouchet.stan@gmail.com>
2020-10-29 10:41:23 +0100

    exo javascript

---
+++ Documents/javascriptexercice/script.js
@@ -0,0 +1,35 @@
+/*
+var number=5;
+var text="test";
+var bool=false;
+
+
+console.log(Boolean(0));
+console.log(Boolean(bool));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+console.log(Boolean(""));
+*/
+
+var testArray = ["pomme", "cerise", "banane", "kiwi","litchi"];
+
+var fruit = {
+    couleur: "rouge",
+    longueur: "grand",
+    mure: "vrai",
+    pays: "japon"          
+           
+}
+
+
+console.log(testArray);
+
+
+
+
+
+
+
+
+
